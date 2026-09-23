1e5a562b7012cf5a97b2b17929f57798882581f3
Rooknew <miguelperreira111@gmail.com>
2026-09-23 15:36:27 +0100

    Exercicio2.py

--- João/Exercicio2.py
+++ João/Exercicio2.py
@@ -3,3 +3,9 @@ nome = input("Por favor digite o seu nome : ")
 quantidade = len(nome.replace(" ", ""))
 
 print("O nome do utilizador tem", quantidade, "caracteres, sem contar os espaços.")
+
+if quantidade>=10:
+   print("Numero de Caracter do Utilizador foi Aprovado")
+
+else:
+   print("Numero de Caracter do Utilizador foi Reprovado")
